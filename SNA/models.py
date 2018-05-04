2f5ed7a0e35718102c653ac47e63ca69c7aecb05
AhmedSI <asimk2015@yahoo.com>
2018-05-04 21:59:12 +0200

    secondCommit

--- SNA/models.py
+++ SNA/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class User (models.Model):
 	user_name = models.CharField(max_length = 50)
 	Email = models.EmailField(unique = True)
-	passwrd = models.PassWordField(max_length = 50)
-	age = models.IntegerField(max_length = 3, null = True)
+	#password = models.PasswordField(max_length = 50)
+	age = models.IntegerField(null = True)
 	phone_num = models.IntegerField(unique = True,null = True)
 	works_at = models.CharField(max_length = 50,null = True)
 	address = models.CharField(max_length = 50,null=True)
@@ -14,38 +14,38 @@ class User (models.Model):
 	def __str__ (self):
 		return self.user_name
 
+class Group (models.Model):
+	name = models.CharField(max_length = 50)
+	admin = models.ForeignKey(User,on_delete = models.CASCADE,related_name = "group_admin")
+
+	def __str__(self):
+		return self.name
 
 class Post (models.Model):
-	choices = 'timeline'
+	Choices = (('T','timeline'),('G','group'))
 	body = models.TextField()
-	user = models.ForiegnKey(User, on_delete = models.CASCADE)
-	scope = models.ChoiceField()
-	group = models.ForiegnKey(Group, on_delete = models.CASCADE)
+	user = models.ForeignKey(User, on_delete = models.CASCADE,related_name = "Post_publisher")
+	scope = models.CharField(max_length = 1,choices = Choices,default = 'T')
+	group = models.ForeignKey(Group, on_delete = models.CASCADE,related_name = "post_group")
 
 	def __str__(self):
 		return self.body
 
 class Comment(models.Model):
-	post = models.ForiegnKey(Post,on_delete = CASCADE)
-	user = models.ForiengKey(User,on_delete = CASCADE)
+	post = models.ForeignKey(Post,on_delete = models.CASCADE)
+	user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = "comment_owner")
 	body = models.TextField()
 
 	def __str__(self):
 		return self.body
 
-class Group (models.Model):
-	name = models.CharField(max_length = 50)
-	post = models.ForiegnKey(models,on_delete = CASCADE)
-	admin = models.ForiegnKey(models,on_delete = CASCADE)
-	user = models.ForiegnKey(models,on_delete = CASCADE)
-
-	def __str__(self):
-		return self.name
 
 class Like(models.Model):
-	user = models.ForiegnKey(User,on_delete = CASCADE)
-	post = models.ForiegnKey(Post,on_delete = CASCADE)
-	comment = models.ForiegnKey(Comment,on_delete = CASCADE)
-
+	Choices = (('P','post'),('C','comment'))
+	user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = "like_owner")
+	post = models.ForeignKey(Post,on_delete = models.CASCADE,related_name = "liked_post")
+	comment = models.ForeignKey(Comment,on_delete = models.CASCADE,related_name = "liked_comment")
+	scope = models.CharField(max_length = 1,choices = Choices,default = 'P')
+	
 	def __str__(self):
 		return self.User
--- dsProject/settings.py
+++ dsProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SNA.apps.SnaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

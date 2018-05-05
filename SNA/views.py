4d40240a1b6058d833c91323582e4bb23b979c5c
AhmedSI <asimk2015@yahoo.com>
2018-05-05 03:06:42 +0200

    login&signup cycle

--- SNA/views.py
+++ SNA/views.py
@@ -4,35 +4,43 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
+	#load start page
 	template = loader.get_template('SNA/index.html')
 	return HttpResponse(template.render({},request))
 
 def signup1(request):
-	template = template = loader.get_template('SNA/signup.html')
-	return HttpResponse(template.render({},"registeration finised log in now"))
+	#load signup page
+	template = loader.get_template('SNA/signup.html')
+	return HttpResponse(template.render({},request))
 
 def signup(request):
+	#process and redirect to login page if new email else redirect to sinup again
+	template = loader.get_template('SNA/signup.html')
 	if request.method == "POST" :
 		email = request.POST.get("email")
-		if register(email) :
+		if register(email ) == True:
 			template = loader.get_template('SNA/login.html')
 		else :
-			return Httpresponse("Email's already been used try another one")
+			
+			return HttpResponse(template.render({},request))
 	else :
-		template = template = loader.get_template('SNA/signup.html')
-	return HttpResponse(template.render({},"registeration finised log in now"))
+		template = loader.get_template('SNA/signup.html')
+	return HttpResponse(template.render({},request))
 
 def register(Email):
-	if Email == "a@a.com" :
-		return false
+	#check if email existed or not
+	if Email == 'a@a.com':
+		return False
 	else :
 		return True
 
 def login(request):
+	#load login page
 	template = loader.get_template('SNA/login.html')
 	return HttpResponse(template.render({},request))
 
 def home(request):
+	#process and redirect to homepage
 	template = loader.get_template('SNA/homepage.html')
 		
 	if request.method == "POST":
